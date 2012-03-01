b44aa40a84893eccdf26a829633d1ec04e337cab
heguangyu5 <heguangyu5@gmail.com>
2012-03-01 10:36:29 +0800

    use mock object

--- RealWorldSolutionsForDevelopingHighQualityPHPFrameworksAndApplications/MockUsersTableDataGateway.php
+++ RealWorldSolutionsForDevelopingHighQualityPHPFrameworksAndApplications/MockUsersTableDataGateway.php
@@ -6,6 +6,16 @@ class MockUsersTableDataGateway extends TableDataGateway
 {
     public function findUserByEmail($email)
     {
-        return FALSE;
+        return array(
+            'id'    => 42,
+            'username' => '何广宇',
+            'email' => 'heguangyu5',
+            'code' => NULL
+        );
+    }
+
+    public function updateUserWithConfirmationCode($email, $code)
+    {
+        throw new PDOException();
     }
 }
--- RealWorldSolutionsForDevelopingHighQualityPHPFrameworksAndApplications/UserControllerTest.php
+++ RealWorldSolutionsForDevelopingHighQualityPHPFrameworksAndApplications/UserControllerTest.php
@@ -1,17 +1,13 @@
 <?php
 require_once 'UserController.php';
 require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'UsersTableDataGateway.php';
+require_once 'MockUsersTableDataGateway.php';
 
 class UserControllerTest extends PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
-        $db = new PDO('sqlite::memory:');
-        $db->exec(file_get_contents('schema.sql'));
-        $db->exec("insert into Users(username, email) values('何广宇', 'heguangyu5')");
-
-        $this->gateway = new UsersTableDataGateway($db);
+        $this->gateway = new MockUsersTableDataGateway();
         $this->controller = new UserController($this->gateway);
     }
 
@@ -34,5 +30,12 @@ class UserControllerTest extends PHPUnit_Framework_TestCase
         $_POST['email'] = 'heguangyu5';
         $view = $this->controller->resetPasswordAction();
         $this->assertInstanceOf('View', $view);
+    }
+
+    public function testDisplaysErrorViewWhenNoUserFound()
+    {
+        $_POST['email'] = 'nosense';
+        $result = $this->controller->resetPasswordAction();
+        $this->assertInstanceOf('ErrorView', $result);
     }
 }
